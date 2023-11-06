c1e4a056c9a0b6dec8e6138cb2a096b02f5866bb
Norman-Hu <norman.hutte@gmail.com>
2023-11-06 17:08:12 +0100

    Add Ray class

---
+++ include/Ray.h
@@ -0,0 +1,18 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include <Vec3.h>
+
+class Ray
+{
+public:
+	inline Ray(const Vec3 & _origin, const Vec3 & _direction)
+	: origin(_origin), direction(_direction)
+	{
+	}
+
+public:
+	Vec3 origin, direction;
+};
+
+#endif
